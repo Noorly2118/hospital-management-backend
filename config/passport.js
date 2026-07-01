354a4afcca1cce71f43c21598024af913b543595
Noorly <noorly21118@gmail.com>
2026-07-01 22:43:40 +0300

    user fixed

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import bcrypt from "bcryptjs";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 // Hardcoded environment variables
 const env = {
